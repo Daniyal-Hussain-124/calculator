9c9096dd89e793d32f1ddfd2bf23d36412bc57ee
Afraz-Khan1 <KHAN4700197@cloud.neduet.edu.pk>
2024-11-13 17:39:19 +0500

    Update code.c
    updated Code

--- code.c
+++ code.c
@@ -2,7 +2,7 @@
 int main(void)
 {
     char option ;
-    printf("Choose the arithmetic operator (+, -, *, /) :");
+    printf("Choose the arithmetic operator (+, -, *, /,%) :");
     scanf("%c", &option);
 
     int a, b ;
@@ -10,12 +10,24 @@ int main(void)
     switch (option)
     {
     case ('+'):{
-        printf("Enter the numbers to be added: ");
+        printf("Enter the numbers to be added: \n");
         scanf("%d\n%d", &a, &b);
         int c = a + b ;
         printf("The sum is: %d", c);
         break;
     }
+        case ('-'):{
+        printf("Enter the number to be subtracted: \n");
+        scanf("%d\n%d",&a,&b);
+        int c= a-b;
+        printf("The difference is %d \n",c);
+        }
+        case ('*'):{
+        printf("Enter the number to be multiplied: \n");
+        scanf("%d\n%d",&a,&b);
+		int c= a*b;
+		printf("The multiplication answer is %d \n",c);    
+        }
     default:
         break;
     }
