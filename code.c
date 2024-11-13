275bbff5237e528e755823e26c45d8bb702fd465
Saim Saqib <saimmoin64@gmail.com>
2024-11-13 18:33:24 +0500

    added divide case

--- code.c
+++ code.c
@@ -1,22 +1,46 @@
 #include <stdio.h>
 int main(void)
 {
-    char option ;
+    char option;
     printf("Choose the arithmetic operator (+, -, *, /,%) :");
     scanf("%c", &option);
 
-    int a, b ;
+    int a, b;
 
     switch (option)
     {
-    case ('+'):{
+    case ('+'):
+    {
         printf("Enter the numbers to be added: \n");
         scanf("%d\n%d", &a, &b);
-        int c = a + b ;
+        int c = a + b;
         printf("The sum is: %d", c);
         break;
     }
-        
+    case ('-'):
+    {
+        printf("Enter the number to be subtracted: \n");
+        scanf("%d\n%d", &a, &b);
+        int c = a - b;
+        printf("The difference is %d \n", c);
+    }
+    case ('*'):
+    {
+        printf("Enter the number to be multiplied: \n");
+        scanf("%d\n%d", &a, &b);
+        int c = a * b;
+        printf("The multiplication answer is %d \n", c);
+    }
+
+    case ('/'):
+    {
+        printf("Enter the numbers to be divided: \n");
+        scanf("%d\n%d", &a, &b);
+        int c = a / b;
+        printf("The quotient is: %d", c);
+        break;
+    }
+
     default:
         break;
     }
