f21510b765c0ffd6eebdaca2d8377d3f2193ed47
Daniyal-Hussain-124 <danyalsodhar5@gmail.com>
2024-11-13 14:07:49 +0500

    addition feature

--- code.c
+++ code.c
@@ -4,4 +4,19 @@ int main(void)
     char option ;
     printf("Choose the arithmetic operator (+, -, *, /) :");
     scanf("%c", &option);
+
+    int a, b ;
+
+    switch (option)
+    {
+    case ('+'):{
+        printf("Enter the numbers to be added: ");
+        scanf("%d\n%d", &a, &b);
+        int c = a + b ;
+        printf("The sum is: %d", c);
+        break;
+    }
+    default:
+        break;
+    }
 }
